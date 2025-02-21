cfd01a5100c922079ed18854fb7d3c982ac64a0a
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-21 17:01:24 -0300

    configurando translate.ts

--- src/services/translate.ts
+++ src/services/translate.ts
@@ -1,3 +1,28 @@
 import { Configuration, OpenAIApi } from "openai";
+import { SUPPORTED_LANGUAGES } from "../constants/constants";
+import { FromLanguage, Language } from "../types.d";
 
 const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+const configuration = new Configuration({ apiKey });
+const openai = new OpenAIApi(configuration);
+
+export async function tranlate({
+  fromLanguage,
+  toLanguage,
+  text,
+}: {
+  fromLanguage: FromLanguage;
+  toLanguage: Language;
+  text: string;
+}) {
+  console.log(openai);
+
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a AI that translates text. You receive a text from the user. Do not answer, just translate the text. The original language is surrounded by `{{` and `}}`. You can also receive {{auto}} which means that you have to detect the language. You can translate to any language. The language you translate to is surrounded by `[[` and `]]`.",
+    },
+  ];
+}
