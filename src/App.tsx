4e8431b58b55f6e026d4774e6a36c85bb9218934
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-21 09:02:08 -0300

    creando estado inicial y reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+const initialState = {
+  fromLanguage: "auto",
+  toLanguage: "en",
+  fromText: "",
+  result: "",
+  loading: false,
+};
+
+function reducer(state, action) {}
+
 function App() {
   return (
     <div className="App">
       <h1>Translator</h1>
+      <p>{initialState.fromLanguage}</p>
     </div>
   );
 }
