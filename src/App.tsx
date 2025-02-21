439d04650e79ea10b224bdf215bb638934d82302
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-21 10:10:39 -0300

    exportando constantes y funciones desde useStore hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { useReducer } from "react";
-import { type State, type Action } from "./types";
-
-const initialState: State = {
-  fromLanguage: "auto",
-  toLanguage: "en",
-  fromText: "",
-  result: "",
-  loading: false,
-};
-
-function reducer(state: State, action: Action) {
-  const { type } = action;
-
-  if (type === "INTERCHANGE_LANGUAGES") {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage,
-    };
-  }
-
-  if (type === "SET_FROM_LANGUAGE") {
-    return {
-      ...state,
-      fromLanguage: action.payload,
-    };
-  }
-
-  if (type === "SET_TO_LANGUAGE") {
-    return {
-      ...state,
-      toLanguage: action.payload,
-    };
-  }
-
-  if (type === "SET_FROM_TEXT") {
-    return {
-      ...state,
-      loading: true,
-      fromText: action.payload,
-      result: "",
-    };
-  }
-
-  if (type === "SET_RESULT") {
-    return {
-      ...state,
-      loading: false,
-      result: action.payload,
-    };
-  }
-
-  return state;
-}
+import { useStore } from "./hooks/useStore";
 
 function App() {
-  const [{ fromLanguage, toLanguage, fromText, result, loading }, dispatch] =
-    useReducer(reducer, initialState);
+  const { fromLanguage, setFromLanguage } = useStore();
 
   return (
     <div className="App">
       <h1>Translator</h1>
       <p>{fromLanguage}</p>
-      <button
-        onClick={() => {
-          dispatch({ type: "SET_FROM_LANGUAGE", payload: "es" });
-        }}
-      >
-        Cambiar a Español
-      </button>
+      <button onClick={setFromLanguage}>Cambiar a Español</button>
     </div>
   );
 }
